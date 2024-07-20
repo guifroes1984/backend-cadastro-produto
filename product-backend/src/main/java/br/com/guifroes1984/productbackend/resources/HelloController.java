ebd016857889323b13998408b6eec8e79bb5f505
guifroes1984 <guilhermefroes26@gmail.com>
2024-07-20 02:08:04 -0300

    Iniciando repo

---
+++ product-backend/src/main/java/br/com/guifroes1984/productbackend/resources/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.guifroes1984.productbackend.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("hello")
+	public String getHello() {
+		return "Hello Spring Boot";
+	}
+	
+}
