25bc1fe93c05d447a771714d21b1d1c683bf1fb9
guifroes1984 <guilhermefroes26@gmail.com>
2024-08-03 01:23:05 -0300

    Ajuste feitos para acompanhar o Front

---
+++ product-backend/src/main/java/br/com/guifroes1984/productbackend/models/Produto.java
@@ -0,0 +1,92 @@
+package br.com.guifroes1984.productbackend.models;
+
+public class Produto {
+
+	private int id;
+	private String nome;
+	private String descricao;
+	private Categoria categoria;
+	private boolean promocao;
+	private boolean novoProduto;
+
+	private double preco;
+
+	public Produto(int id, String nome, double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Produto(int id, String nome, String descricao, Categoria categoria, boolean promocao, boolean novoProduto,
+			double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.categoria = categoria;
+		this.promocao = promocao;
+		this.novoProduto = novoProduto;
+		this.preco = preco;
+	}
+
+	public Produto() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public boolean isPromocao() {
+		return promocao;
+	}
+
+	public void setPromocao(boolean promocao) {
+		this.promocao = promocao;
+	}
+
+	public boolean isNovoProduto() {
+		return novoProduto;
+	}
+
+	public void setNovoProduto(boolean novoProduto) {
+		this.novoProduto = novoProduto;
+	}
+
+}
---
+++ product-backend/src/main/java/br/com/guifroes1984/productbackend/resources/ProdutoController.java
@@ -0,0 +1,55 @@
+package br.com.guifroes1984.productbackend.resources;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.guifroes1984.productbackend.models.Produto;
+
+@RestController
+@CrossOrigin
+public class ProdutoController {
+
+	private List<Produto> produtos = new ArrayList<>();
+	
+	@PostMapping("produtos")
+	public ResponseEntity<Produto> salvar(@RequestBody Produto produto) {
+		produto.setId(produtos.size() + 1);
+		produtos.add(produto);
+		
+		URI local = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(produto.getId())
+				.toUri();
+		
+		return ResponseEntity.created(local).body(produto);
+	}
+
+	@GetMapping("produtos/{id}")
+	public ResponseEntity<Produto> getProduto(@PathVariable int id) {
+		Produto prod = produtos.stream()
+								.filter(p -> p.getId() == id)
+								.findFirst()
+								.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado."));
+		
+		return ResponseEntity.ok(prod);
+	}
+
+	@GetMapping("produtos")
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+}
