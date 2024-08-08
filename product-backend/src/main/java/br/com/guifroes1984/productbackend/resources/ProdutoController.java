19163fb06743cf930f5a653b8ee721282f7a0e2b
guifroes1984 <guilhermefroes26@gmail.com>
2024-08-08 00:14:03 -0300

    Implementado ProdutoService

--- product-backend/src/main/java/br/com/guifroes1984/productbackend/resources/ProdutoController.java
+++ product-backend/src/main/java/br/com/guifroes1984/productbackend/resources/ProdutoController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,13 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.guifroes1984.productbackend.models.Categoria;
 import br.com.guifroes1984.productbackend.models.Produto;
-import br.com.guifroes1984.productbackend.repositories.CategoriaRepository;
-import br.com.guifroes1984.productbackend.repositories.ProdutoRepository;
 import br.com.guifroes1984.productbackend.services.ProdutoService;
 
 @RestController
@@ -28,18 +23,12 @@ import br.com.guifroes1984.productbackend.services.ProdutoService;
 public class ProdutoController {
 	
 	@Autowired
-	public ProdutoRepository produtoRepository;
-	
-	@Autowired
-	public CategoriaRepository categoriaRepository;
-	
-	@Autowired
 	public ProdutoService produtoService;
 	
 	@PostMapping("produtos")
 	public ResponseEntity<Produto> salvar(@RequestBody Produto produto) {
 		
-		produto = produtoRepository.save(produto);
+		produto = produtoService.salvar(produto);
 		
 		URI local = ServletUriComponentsBuilder
 				.fromCurrentRequest()
@@ -58,41 +47,18 @@ public class ProdutoController {
 
 	@GetMapping("produtos")
 	public List<Produto> getProdutos() {
-		return produtoRepository.findAll();
+		return produtoService.getAll();
 	}
 	
 	@DeleteMapping("produtos/{id}")
 	public ResponseEntity<Void> removeProduto(@PathVariable int id) {
-		
-		Produto produto = produtoRepository.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado."));
-		
-		produtoRepository.delete(produto);
-		
+		produtoService.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping("produtos/{id}")
 	public ResponseEntity<Void> atualizarProduto(@PathVariable int id, @RequestBody Produto produtoAtualizado) {
-		
-		Produto produto = produtoRepository.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado."));
-		
-		if (produtoAtualizado.getCategoria() == null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não pode ser vazia");
-		}
-		
-		Categoria categoria = categoriaRepository.findById(produtoAtualizado.getCategoria().getId())
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrado."));
-		
-		produto.setDescricao(produtoAtualizado.getDescricao());
-		produto.setNome(produtoAtualizado.getNome());
-		produto.setPreco(produtoAtualizado.getPreco());
-		produto.setNovoProduto(produtoAtualizado.isNovoProduto());
-		produto.setPromocao(produtoAtualizado.isPromocao());
-		produto.setCategoria(categoria);
-		
-		produtoRepository.save(produto);
+		produtoService.atualizar(id, produtoAtualizado);
 		return ResponseEntity.ok().build();
 	}
 
--- product-backend/src/main/java/br/com/guifroes1984/productbackend/services/ProdutoService.java
+++ product-backend/src/main/java/br/com/guifroes1984/productbackend/services/ProdutoService.java
@@ -1,11 +1,15 @@
 package br.com.guifroes1984.productbackend.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import br.com.guifroes1984.productbackend.models.Categoria;
 import br.com.guifroes1984.productbackend.models.Produto;
+import br.com.guifroes1984.productbackend.repositories.CategoriaRepository;
 import br.com.guifroes1984.productbackend.repositories.ProdutoRepository;
 
 @Service
@@ -14,11 +18,47 @@ public class ProdutoService {
 	@Autowired
 	public ProdutoRepository produtoRepository;
 	
+	@Autowired
+	public CategoriaRepository categoriaRepository;
+	
 	public Produto getById(int id) {
 		Produto produto = produtoRepository.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado."));
 		
 		return produto;
+	}
+	
+	public List<Produto> getAll() {
+		return produtoRepository.findAll();
+	}
+	
+	public Produto salvar(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	public void deleteById(int id) {
+		Produto produto = getById(id);
+		produtoRepository.delete(produto);
+	}
+	
+	public void atualizar(int id, Produto produtoAtualizado) {
+		Produto produto = getById(id);
+		
+		if (produtoAtualizado.getCategoria() == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não pode ser vazia");
+		}
+		
+		Categoria categoria = categoriaRepository.findById(produtoAtualizado.getCategoria().getId())
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrado."));
+		
+		produto.setDescricao(produtoAtualizado.getDescricao());
+		produto.setNome(produtoAtualizado.getNome());
+		produto.setPreco(produtoAtualizado.getPreco());
+		produto.setNovoProduto(produtoAtualizado.isNovoProduto());
+		produto.setPromocao(produtoAtualizado.isPromocao());
+		produto.setCategoria(categoria);
+		
+		produtoRepository.save(produto);
 	}
 
 }
