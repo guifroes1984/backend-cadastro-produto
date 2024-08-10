174bac28e8d08d6ed60a2789528e3fe76cd89766
guifroes1984 <guilhermefroes26@gmail.com>
2024-08-10 00:07:09 -0300

    Melhorando o mapeamento das entidades

--- product-backend/src/main/java/br/com/guifroes1984/productbackend/models/Categoria.java
+++ product-backend/src/main/java/br/com/guifroes1984/productbackend/models/Categoria.java
@@ -2,6 +2,7 @@ package br.com.guifroes1984.productbackend.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column(nullable = false, unique = true)
 	private String nome;
 
 	public Categoria(Integer id, String nome) {
--- product-backend/src/main/java/br/com/guifroes1984/productbackend/models/Produto.java
+++ product-backend/src/main/java/br/com/guifroes1984/productbackend/models/Produto.java
@@ -2,6 +2,7 @@ package br.com.guifroes1984.productbackend.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,7 +19,11 @@ public class Produto implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false)
 	private String nome;
+	
+	@Column(nullable = false, length = 1024)
 	private String descricao;
 
 	@ManyToOne
